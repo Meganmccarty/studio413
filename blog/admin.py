7e62195feaf52dca7da00516a69b8cc02f1a1902
Megan McCarty <megan_mccarty@hotmail.com>
2020-02-29 20:55:01 +0000

    Modified link to admin and made admin interface more user-friendly

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    search_fields = ['title', 'text']
+    fields = ('title', 'author', 'text', 'created_date', 'published_date')
+    #prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'email', 'created_date', 'approved_comment')
+    list_filter = ('approved_comment', 'created_date')
+    search_fields = ('name', 'email', 'text')
+    fields = ('post', 'name', 'email', 'text', 'created_date', 'approved_comment')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approve_comment=True)
