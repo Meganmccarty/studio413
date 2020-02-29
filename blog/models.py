a12dbb0de06bf3292b1cf79a3afb3699bd8997b4
Megan McCarty <megan_mccarty@hotmail.com>
2020-02-29 12:20:07 -0500

    Added slug generation to Post model, added name/email fields to Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django.utils.text import slugify
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    slug = models.SlugField(default='', null=True, blank=True)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
@@ -16,13 +17,20 @@ class Post(models.Model):
     
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
-    
+
+    def save(self, *args, **kwargs):
+        slug = slugify(self.title)
+        self.slug = slug
+        super(Post, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(default='')
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
