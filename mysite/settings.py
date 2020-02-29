f2438d3aa7a7b29f97ad42f90d9e7638faf0e4d0
Megan McCarty <megan_mccarty@hotmail.com>
2020-02-29 22:16:29 +0000

    Collect static

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,6 +56,8 @@ ROOT_URLCONF = 'mysite.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
