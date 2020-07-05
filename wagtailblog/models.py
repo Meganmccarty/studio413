82ccfbb2786b56a1655c2cdd1687e95e9d44b3e5
Megan McCarty <megan_mccarty@hotmail.com>
2020-07-05 15:20:26 -0400

    Added blog page model and template

--- wagtailblog/models.py
+++ wagtailblog/models.py
@@ -2,11 +2,29 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.search import index
 
 class HomePage(Page):
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+    ]
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    author = models.CharField(max_length=250, default='', null=True, blank=True)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('author'),
+            FieldPanel('date'),
+        ], heading="Blog information"),
+        FieldPanel('body'),
     ]
