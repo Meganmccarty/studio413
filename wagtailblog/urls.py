eb75b8f70bdad4061c90280411ec4c350cf79adb
Megan McCarty <megan_mccarty@hotmail.com>
2020-07-07 11:36:38 -0400

    Removed commented out paths; don't need in wagtailblog app

--- wagtailblog/urls.py
+++ wagtailblog/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.home, name='home'),
-    #path('blog/', views.post_list, name='post_list'),
     path('products/', views.products, name='products'),
     path('products/art-for-sale/', views.art_for_sale, name='art_for_sale'),
     path('products/art-pages/', views.art_pages, name='art_pages'),
@@ -11,10 +9,4 @@ urlpatterns = [
     path('confirm/', views.confirm, name='confirm'),
     path('delete/', views.delete, name='delete'),
     path('contact/', views.contact, name='contact'),
-    #path('post/new/', views.post_new, name='post_new'),
-    #path('post/<slug>/', views.post_detail, name='post_detail'),
-    #path('post/<slug>/edit/', views.post_edit, name='post_edit'),
-    #path('drafts/', views.post_draft_list, name='post_draft_list'),
-    #path('post/<slug>/publish/', views.post_publish, name='post_publish'),
-    #path('post/<slug>/remove/', views.post_remove, name='post_remove'),
 ]
