cee7c0bddd9c7c3f36780a14784ea1308b958b7f
Megan McCarty <megan_mccarty@hotmail.com>
2020-07-07 11:26:03 -0400

    Added menu item in page editor; notify by email not working

--- wagtailblog/wagtail_hooks.py
+++ wagtailblog/wagtail_hooks.py
@@ -1,8 +1,58 @@
 import wagtail.admin.rich_text.editors.draftail.features as draftail_features
-from wagtail.admin.rich_text.converters.html_to_contentstate import (
-    InlineStyleElementHandler
-)
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
+from wagtail.admin.action_menu import ActionMenuItem
 from wagtail.core import hooks
+
+from .models import Subscriber, BlogPage
+from django.conf import settings
+from django.core.mail import send_mail
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+
+class NotificationMenuItem(ActionMenuItem):
+    name = 'email-notification'
+    label = "Notify Subscribers of New Post"
+
+    def send(self, request):
+        subscribers = Subscriber.objects.filter(confirmed=True)
+        sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+        for sub in subscribers:
+            message = Mail(
+                from_email=settings.FROM_EMAIL,
+                to_emails=sub.email,
+                subject="New blog post on Studio413!",
+                html_content=(
+                    '<div style="background-color:rgb(0,40,110);border:10px solid rgb(0,40,110);border-radius:10px">' \
+                    '<div style="background-color:rgb(193,222,227);border:10px solid rgb(193,222,227);border-radius:10px">' \
+                    '<div style="background-color:white;border-radius:10px">' \
+                    '<center><h3>Studio413 has published a new blog post!</h3></center>'\
+                    '<br>' \
+                    '<span style="margin-left:10px">Click the following link to read the new post:</span>' \
+                    '<br>' \
+                    '<span style="margin-left:10px"><a href="{}/{}/">{}</a></span>'\
+                    '<br>' \
+                    '<span style="margin-left:10px">Or, you can copy and paste the following url into your browser:</span>' \
+                    '<br>' \
+                    '<span style="margin-left:10px">{}/{}</span>'\
+                    '<br>' \
+                    '<hr><center>If you no longer wish to receive our blog updates, you can ' \
+                    '<a href="{}/?email={}&conf_num={}">unsubscribe</a>.</center><br></div></div></div>').format(
+                        request.build_absolute_uri('/blog'),
+                        self.slug,
+                        self.title,
+                        request.build_absolute_uri('/blog'),
+                        self.slug,
+                        request.build_absolute_uri('/delete'),
+                        sub.email,
+                        sub.conf_num
+                    )
+                )
+            sg.send(message)
+
+@hooks.register('register_page_action_menu_item')
+def register_notification_menu_item():
+    return NotificationMenuItem(order=100)
+
 
 @hooks.register("register_rich_text_features")
 def register_centertext_feature(features):
