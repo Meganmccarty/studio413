2a54982d4a67792dc9bd5250dacf339fea5b2f77
Megan McCarty <megan_mccarty@hotmail.com>
2020-07-09 14:19:56 -0400

    Fixed js issue, moved js to separate file

---
+++ wagtailblog/static/js/scripts.js
@@ -0,0 +1,11 @@
+function deleteEmail() {
+    var message = "{% trans 'Do you really want to remove the selected email address?' %}";
+    var actions = document.getElementsByName('action_remove');
+    if (actions.length) {
+        actions[0].addEventListener("click", function(e) {
+            if (! confirm(message)) {
+                e.preventDefault();
+            }
+        });
+    }
+};
